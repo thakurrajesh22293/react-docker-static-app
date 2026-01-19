699c319d83741387660bc2e962af1aea4307933d
thakurrajesh22293 <139438293+thakurrajesh22293@users.noreply.github.com>
2026-01-19 22:37:47 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>🚀 This is a React.js based project</h1>
+      <h2>Running inside Docker Container</h2>
+      <p>Docker practice project (without Nginx)</p>
+    </div>
+  );
+}
+
+export default App;
